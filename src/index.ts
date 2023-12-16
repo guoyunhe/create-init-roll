dba9aaad7635e14e04a63eb51fe5b36f351a33f3
Guo Yunhe <i@guoyunhe.me>
2023-12-16 21:33:53 +0800

    run migrate

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,8 @@ export async function createInitRoll(project: string | null) {
   const projectFullPath = project ? join(process.cwd(), project) : process.cwd();
 
   const template = new Template(projectFullPath, 'create-init-roll', 'create-init-roll');
+
   template.addMigration(migration20231216);
+
+  await template.migrate();
 }

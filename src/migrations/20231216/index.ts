c5331486871d4f326423db918bfbb8e908a28552
Guo Yunhe <i@guoyunhe.me>
2023-12-17 20:38:40 +0800

    package.json

--- src/migrations/20231216/index.ts
+++ src/migrations/20231216/index.ts
@@ -3,6 +3,11 @@ import { Migration } from 'init-roll';
 export const migration20231216 = new Migration(20231216, async (migration) => {
   await migration.updatePackageJson(async (oldPkg) => {
     const newPkg = { ...oldPkg };
+    if (!newPkg.dependencies) {
+      newPkg.dependencies = {};
+    }
+    newPkg.dependencies['init-roll'] = '1.x';
+    newPkg.dependencies['commander'] = '11.x';
     return newPkg;
   });
 });
